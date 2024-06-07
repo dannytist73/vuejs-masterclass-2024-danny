944f5b1d0e3a82bc69fe206cb1dcbd860d689525
danny_boy <dannytist.73@gmail.com>
2024-06-08 01:15:39 +0800

    Added Supabase Configurations

---
+++ src/pages/projects/index.vue
@@ -0,0 +1,12 @@
+<template>
+  <div>
+    <h1>Projects</h1>
+    <RouterLink to="/"> Go to home </RouterLink>
+  </div>
+</template>
+  
+  <script setup lang="ts">
+</script>
+  
+  <style>
+</style>
